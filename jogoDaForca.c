19645fb34bb83d3e3ba61dd1396a847f006699e1
Guilherme Wallace <guilherme.wallace13@hotmail.com>
2025-01-11 21:01:06 -0300

    Fazendo o jogo da Forma em C

---
+++ jogoDaForca.c
@@ -0,0 +1,49 @@
+// Jogo da Forca -> ---- -- ----- :D
+// passo 1 - fazer a Main -> Introduzá o game 
+// passo 2 - função forca -> Fazá a lógica do game enquanto chama as funções de verificação
+// passo 3 - funções auxiliares de verificação
+// fim - correção de bugs e deixar o jogo bonitinho (fazer função para limpar o console)
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+void limparTela() {
+    system("cls");
+}
+
+
+void forca() {
+    int maximoDeTentativas = 6;
+    char palavra[100];
+    char acertos[52];
+    char letrasTentadas[52];
+    int tentativas = 0;
+    int erros = 0;
+
+    printf("Informa a pavrada secreta: ");
+    scanf("%s", palavra);
+    getchar();
+
+    limparTela();
+
+    initAcertos(acertos, strlen(palavra));
+
+    while (erros < maximoDeTentativas) {
+        limparTela();
+
+    }
+    
+}
+
+int main() {
+    char restart;
+    do {
+        forca();
+        printf("\nQuer jogar de novo? (s/n): ");
+        scanf(" %c", &restart);
+    } while (tolower(restart) == 's');
+
+    printf("bye bye! \n");
+    return 0;    
+}
